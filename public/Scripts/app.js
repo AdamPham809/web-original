aa70c5a00eaf4cb50ba76369d9de76b849d4e480
AdamPham809 <113602112+AdamPham809@users.noreply.github.com>
2022-11-21 20:04:47 -0500

    First Commit

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -3,6 +3,18 @@
     function Start()
     {
         console.log("App Started");
+        let deleteButton = document.querySelectorAll('.btn-danger');
+    for(button of deleteButton)
+    {
+        button.addEventListener('click',(event)=>{
+            if(!confirm("Are you sure?"))
+            {
+                event.preventDefault();
+                window.location.assign('/book-list');
+            }
+        });
+    }
     }
     window.addEventListener("load", Start);
+    
 })();
